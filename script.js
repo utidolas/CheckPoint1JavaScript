ab2e48158fcbf0ee7391c5f1d33c143c497cb6a9
Caiua Utida <utidapro@gmail.com>
2023-04-06 17:01:23 -0300

    Segundo commit com algumas questões

---
+++ script.js
@@ -0,0 +1,28 @@
+// 1 - Declaring var with undefined value:
+/*
+
+let undef_var = undefined;
+console.log(undef_var, typeof(undef_var));
+
+// 3 - Computing IMC
+function computing_bmi (weight, height) {
+    BMI = (weight / height / height) * 10000;
+    return BMI.toFixed(2);
+}  
+
+bmi_result = computing_bmi(70, 170);
+
+if (bmi_result < 18.5){
+    console.log(`Your BMI is ${bmi_result}.
+You are underweight.`);
+}else if (bmi_result <= 24.9){
+    console.log(`Your BMI is ${bmi_result}.
+You are normal.`);
+}else{
+    console.log(`Your BMI is ${bmi_result}.
+You are overweight.`);
+}
+*/
+// 5 - Checking username and password
+var username = prompt("Insert your username: ");
+var password = prompt("Insert your password: ");
