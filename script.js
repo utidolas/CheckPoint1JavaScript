216cacee70f8ab1e1014829d6b2ff87369b37605
Caiua Utida <utidapro@gmail.com>
2023-04-08 17:30:59 -0300

    commit com 6 7 8 9 resolvidos

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 // 6 - Computing avarage
 let grade1 = prompt("Input the 1st grade:");
@@ -37,44 +36,4 @@ let changing_var = "245.3248";
 changed_var = parseFloat(changing_var);
 document.write(changed_var, "<br>");
 document.write(changed_var.toFixed(2));
-=======
-/*
-// 6 - Computing avarage
-let grade1 = prompt("Input the 1st grade:");
-let grade2 = prompt("Input the 2nd grade:");
-let grade3 = prompt("Input the 3th grade:");
-let grade4 = prompt("Input the 4th grade:");
-let grade5 = prompt("Input the 5th grade:");
-let grade6 = prompt("Input the 6th grade:");
-let grade7 = prompt("Input the 7th grade:");
-
-let avarage = (grade1 + grade2 + grade3 + grade4 + grade5 + grade6 + grade7) / 7;
-
-if(avarage >= 6 ){
-    document.write("Congrats!!! you were approved.")
-}else{
-    document.write("Oh, you failed.")
-}
-
-// 7 - User info
-let name = prompt("Input your name: ");
-let age = prompt("Input your age: ");
-let course = prompt("Input your course: ");
-let year = prompt("Input your current year of college: ");
-
-document.write("Name: ", name, "<br>");
-document.write("Age: ", age, "<br>");
-document.write("Course: ", course, "<br>");
-document.write("Year: ", year, "<br>");
-
-// 8 - Changing words
-let phrase = "O LUGAR VIRA TECNOLOGIA";
-document.write(phrase.replace("LUGAR", "MUNDO"));
-
-// 9 - StringToFloat
-let changing_var = "245.3248";
-changed_var = parseFloat(changing_var);
-document.write(changed_var, "<br>");
-document.write(changed_var.toFixed(2));
->>>>>>> f0e229a2a494db0a79ea522a47766efa95b16800
 */
