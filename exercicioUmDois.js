8f629020bb77d1b7746ab2113d0b0fc2f8254924
feelps04 <81180940+feelps04@users.noreply.github.com>
2023-04-09 11:40:19 -0300

    Add files via upload

---
+++ exercicioUmDois.js
@@ -0,0 +1,14 @@
+// exercicio 1
+let nome;
+console.log(nome);
+
+
+
+// exercicio 2 
+var numero = 10;
+if (numero % 2 ==0){
+    console.log(numero + "É um numero par.")
+}
+else{
+    console.log(numero + "É um numero impar")
+}
